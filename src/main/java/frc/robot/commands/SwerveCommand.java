0649c3e24cc0742238b61ba5fc5df4176fb6e08b
GalenSpooner <gspooner@cusd.me>
2024-01-08 17:25:57 -0800

    removed commandbase (o7 you will be missed)

--- src/main/java/frc/robot/commands/SwerveCommand.java
+++ src/main/java/frc/robot/commands/SwerveCommand.java
@@ -8,6 +8,7 @@ import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.Constants;
 import frc.robot.subsystems.SwerveSubsystem;
@@ -19,7 +20,7 @@ import swervelib.math.SwerveMath;
 /**
  * An example command that uses an example subsystem.
  */
-public class SwerveCommand extends CommandBase
+public class SwerveCommand extends Command
 {
 
   private final SwerveSubsystem swerve;
